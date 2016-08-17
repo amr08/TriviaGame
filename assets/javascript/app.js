9c1c31148b3a39d0a45bf275589fbe1780a05a1a
Andrea R <amrdev08@gmail.com>
2016-08-17 19:23:15 -0400

    questionable update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,190 +23,14 @@ function gameStart() {
 
  	$("aside").hide();
  	$("article, section").show();
-    nextQuestion();
     event.stopPropagation();
+    run();
      });
 };
 //end game start
 
 
-
-
-
-function questions (prompt1) {
-   $("#questions").text(prompt1);
-
-
-};
-
-function answers (a,b,c,d) {
-$("#answer1").text(a);
-$("#answer2").text(b);
-$("#answer3").text(c);
-$("#answer4").text(d);
-   
- 
-};
-
-
-
-
-//timer section
-
-run();
-
-
-
-var number = 20;
-
-	function run() {
-
-    	counter = setInterval(timeLeft, 1000);
-	}
-
-
-	function timeLeft() {
-  		number--
- 		$('#timer').html('<h2>' + number + '</h2>');
-
- 		if(number === 0) {
- 			stop();
-	        timeRanOut++
-            nextQuestion();
- 		}
-
-};
-
-
-function stop(){
-		clearInterval(counter);
-
-};
-
-function end(){
-		clearInterval(counter);
-
-};
-
-//end timer section
-
-
-
-
-function correct(answer){
-
-$("section ul a").on("click",function(event) {
-	
-
-	var userPick = ($(this).text());
-    console.log(userPick)
-
- if (userPick == answer) {
- console.log ("you got it!")
- console.log(win++)
-madeSelection++
- nextQuestion();
- 
- }
-
-else  {
-	console.log(loss++)
-	console.log("incorrect");
-	nextQuestion();
-	madeSelection++
-	
-
-
-   }
-
-   
-event.stopPropagation();
-
-
-});
-
-};
-
-
-
-function nextQuestion(){
-
-    questions(quiz[0].question);
-  	answers(quiz[0].answer[0], quiz[0].answer[1], quiz[0].answer[2], quiz[0].answer[3]);
-        number = 20;
-	     run();
-	 correct(quiz[0].correct)
-
-
-
-if(timeRanOut || madeSelection === 1) {
-	   
-		questions(quiz[1].question);
-		var picture = "<img src='https://wildrovertours.com/wp-content/uploads/2015/12/Cliffs-of-Moher-5.jpg'>"
-        document.querySelector("#images").innerHTML = picture; 
-        answers(quiz[1].answer[0], quiz[1].answer[1], quiz[1].answer[2], quiz[1].answer[3]);
-        
-        correct(quiz[1].correct);
-}
-
-
-else if(timeRanOut || madeSelection=== 2) {
-
-        
-		questions(quiz[2].question);
-        answers(quiz[2].answer[0], quiz[2].answer[1], quiz[2].answer[2], quiz[2].answer[3]);
-        $("#images").hide();
-        correct(quiz[2].correct);
-        
-
-}
-
-else if(timeRanOut || madeSelection=== 3) {
-
-
-		questions(quiz[3].question);
-        answers(quiz[3].answer[0], quiz[3].answer[1], quiz[3].answer[2], quiz[3].answer[3]);
-        correct(quiz[3].correct);
-
-}
-
-else if(timeRanOut || madeSelection=== 4) {
-
-   
-		questions(quiz[4].question);
-		var picture = "<img src='http://travelchannel.sndimg.com/content/dam/images/travel/fullset/2015/10/12/new-seven-wonders-machu-picchu.jpg.rend.tccom.616.462.jpeg'>"
-        document.querySelector("#images").innerHTML = picture; 
-        answers(quiz[4].answer[0], quiz[4].answer[1], quiz[4].answer[2], quiz[4].answer[3]);
-        $("#images").show();
-        correct(quiz[4].correct);
-
-}
-
-
- else if(timeRanOut || madeSelection===5) {
-	$("#images").hide();
-	$("#questions").text("wins" + win + "losses" + loss);
-	end();
-	$("section").remove();
-	$("#timer").remove();
-
-
-
-// else if(questionNumber === 5) {
-
-// 		$("#images").remove();
-// 		questions(quiz[5].question);
-// 	    answers(quiz[5].answer);
-        
-
-};
-end();
-
-};
-
-
-
-
+//content
 
 var quiz = [{
 
@@ -242,15 +66,212 @@ var quiz = [{
 },
 ]
 
+//end content 
+
+//content html display
+function questions (prompt1) {
+   $("#questions").text(prompt1);
+
+
+};
+
+function answers (a,b,c,d) {
+$("#answer1").text(a);
+$("#answer2").text(b);
+$("#answer3").text(c);
+$("#answer4").text(d);
+   
+ 
+};
+
+//end content html display
+
+
+//timer section
+
+var number = 20;
+
+	function run() {
+
+    	counter = setInterval(timeLeft, 1000);
+	}
+
+
+function stop(){
+		clearInterval(counter);
+
+};
+
+
+	function timeLeft() {
+  		number--
+ 		$('#timer').html('<h2>' + number + '</h2>');
+
+ 		if(number === 0) {
+ 			stop();
+	      timeRanOut++
+          number = 20;
+            run();
+            
+       
+ 		}
 
 
 
 
 
 
+nextQuestion();
 
 
-//correct indicator
+
+
+
+// function end(){
+// 		clearInterval(counter);
+
+// };
+
+//end timer section
+
+};
+
+
+
+ function correct(answer){
+
+ $("section ul a").on("click",function(event) {
+	madeSelection++
+
+	var userPick = ($(this).text());
+     console.log(userPick)
+      
+
+ if (userPick !== answer) {
+
+
+  //nextQuestion();
+
+  }
+
+  else  {
+// // // 	console.log(loss++)
+// // // 	console.log("incorrect");
+//nextQuestion();
+  	
+	
+	 console.log ("you got it!")
+//  console.log(win++)
+// madeSelection++
+// nextQuestion();
+
+   }
+
+   
+ event.stopPropagation();
+ console.log(madeSelection);
+
+});
+
+};
+
+console.log(madeSelection);
+// });
+
+// };
+
+// function nextQuestion() {
+
+
+// if(number === 0) {
+ 		
+// 	       timeRanOut++
+
+//             //nextQuestion();
+            
+//              number = 10;
+             
+//  		}
+
+
+
+
+function nextQuestion() {
+
+if(timeRanOut === 0) {
+    questions(quiz[0].question);
+  	answers(quiz[0].answer[0], quiz[0].answer[1], quiz[0].answer[2], quiz[0].answer[3]);
+     
+	     
+	 correct(quiz[0].correct)
+
+}
+
+  if(timeRanOut  === 1) {
+	   
+	   
+	   
+		questions(quiz[1].question);
+		var picture = "<img src='https://wildrovertours.com/wp-content/uploads/2015/12/Cliffs-of-Moher-5.jpg'>"
+        document.querySelector("#images").innerHTML = picture; 
+        answers(quiz[1].answer[0], quiz[1].answer[1], quiz[1].answer[2], quiz[1].answer[3]);
+        
+        correct(quiz[1].correct);
+}
+
+
+  if(timeRanOut ===2) {
+
+        
+		questions(quiz[2].question);
+        answers(quiz[2].answer[0], quiz[2].answer[1], quiz[2].answer[2], quiz[2].answer[3]);
+        $("#images").hide();
+        correct(quiz[2].correct);
+        
+
+}
+
+  if(timeRanOut ===3) {
+
+
+		questions(quiz[3].question);
+        answers(quiz[3].answer[0], quiz[3].answer[1], quiz[3].answer[2], quiz[3].answer[3]);
+        correct(quiz[3].correct);
+
+}
+
+  if(timeRanOut === 4) {
+
+   
+		questions(quiz[4].question);
+		var picture = "<img src='http://travelchannel.sndimg.com/content/dam/images/travel/fullset/2015/10/12/new-seven-wonders-machu-picchu.jpg.rend.tccom.616.462.jpeg'>"
+        document.querySelector("#images").innerHTML = picture; 
+        answers(quiz[4].answer[0], quiz[4].answer[1], quiz[4].answer[2], quiz[4].answer[3]);
+        $("#images").show();
+        correct(quiz[4].correct);
+
+}
+
+
+ else if(timeRanOut ===5) {
+	$("#images").hide();
+	$("#questions").text("wins" + win + "losses" + (5-win));
+	end();
+	$("section").remove();
+	$("#timer").remove();
+
+
+}
+
+};
+// else if(questionNumber === 5) {
+
+// 		$("#images").remove();
+// 		questions(quiz[5].question);
+// 	    answers(quiz[5].answer);
+        
+
+//};
 
 
 
