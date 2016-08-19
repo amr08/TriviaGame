f8fb3d7b790b3c59657fadc96586d70ba422f293
Andrea R <amrdev08@gmail.com>
2016-08-19 10:09:58 -0400

    functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 
 
- var timeRanOut = 0;
+ timeRanOut = 0;
 
  var missed = 0;
  var win= 0;
  var miss=0;
+ //var noAnswer= 0;
 
 
 
@@ -52,22 +53,22 @@ var quiz = [{
 	correct: "Times Square"
 },
 
-{ 
+{
 	question: "Where was Leonardo DiCaprio's movie, 'The Beach', filmed?",
 	answer: ["Fiji", "Hawaii", "Thailand", "Costa Rica"],
 	correct: "Thailand"
 
 },
 
-{ 
+{
 	question: "Where is this Wonder of the World Located?",
 	answer: ["Scotland","Mexico", "Peru", "Indonesia"],
 	correct: "Peru"
 
 },
 ]
- 
-//end content 
+
+//end content
 
 //content html display
 function questions (prompt1) {
@@ -81,8 +82,8 @@ $("#answer1").text(a);
 $("#answer2").text(b);
 $("#answer3").text(c);
 $("#answer4").text(d);
-   
- 
+
+
 };
 
 //end content html display
@@ -90,87 +91,82 @@ $("#answer4").text(d);
 
 //timer section
 
-var number = 10;
+var number = 5;
+// var timeup = 3;
 
 	function run() {
 
     	counter = setInterval(timeLeft, 1000);
 	}
 
-
-function stop(){
+	
+	function stop(){
 		clearInterval(counter);
+	}
 
-};
+// 	function restart() {
 
+//     	ticker = setInterval(anotherTicker, 1000);
+// 	}
+
+// 	function hold(){
+// 		clearInterval(ticker);
+// 	}
+// 	function anotherTicker() {
+// 	timeup--
+// 	if(timeup === 0) {
+		
+// 		noAnswer++
+// 		number = 10;
+// 		run();
+// 		nextQuestion();
+// 		console.log(noAnswer)
+// 		hold();
+// 		$("#timeup").empty();
+// 	}
+// }
 
 	function timeLeft() {
-  		number--
+  	number--
  		$('#timer').html('<h2>' + number + '</h2>');
-
  		if(number === 0) {
- 			
-
  			stop();
-	        timeRanOut++;
-	        console.log(timeRanOut)
-            $("#questions").text("Time is up!");
-	        //return
-            number = 10;
-            nextQuestion();
-            run();
- 
-            
- 		}
+      		timeRanOut++;
+      		number = 5;	
+      		run();
+      		nextQuestion();
+  }
+ 	};
 
 //end timer section
 
-};
+
 
 
 
 //check answer
- function correct(answer){
- 	$("section ul a").on("click",function(event) {
-	var userPick = ($(this).text());
-    console.log(userPick)
-      
- 
- 		if (userPick == answer) {
+ $("section ul a").on("click",function(event) {
+    event.stopPropagation();
+  	var userPick = ($(this).text());
+    var correct = quiz[timeRanOut].correct;
 
-  			
+ 		if (userPick === correct) {
   			win++
   			number = 5;
   			$("#correct").append("<img src='http://www.marymarcusfiction.com/wp-content/uploads/2015/09/yay-54383329058.jpeg'/>");
-            $("section ul a").hide();
-          
-  			return questions("Correct!");
-  			
-			 }
-        else if (userPick !== answer) {
-        	miss++;
-        	number = 5;
-        	$("#incorrect").append("<img src='https://upload.wikimedia.org/wikipedia/commons/3/3b/Paris_Tuileries_Garden_Facepalm_statue.jpg'/>");
-            $("section ul a").hide();
-        	return questions("Wrong! Correct answer was " + answer);
-        }
-  		return
-// // // 	console.log(loss++)
-// // // 	console.log("incorrect");
+        $("section ul a").hide();
+  			questions("Correct!");
+		 } 
 
-  	 
+		 else {
+      	miss++;
+      	number = 5;
+      	$("#incorrect").append("<img src='https://upload.wikimedia.org/wikipedia/commons/3/3b/Paris_Tuileries_Garden_Facepalm_statue.jpg'/>");
+        $("section ul a").hide();
+        questions("Wrong! Correct answer was " +  quiz[timeRanOut].correct);
+      }
+  });
 
-
-   
-	event.stopPropagation();
- 
-    
-});
-
-
-return
-
-};
 
 //end checkanswer
 
@@ -179,72 +175,55 @@ return
 if(timeRanOut === 0) {
     questions(quiz[0].question);
   	answers(quiz[0].answer[0], quiz[0].answer[1], quiz[0].answer[2], quiz[0].answer[3]);
-	 correct(quiz[0].correct);
-	 
-
 }
 
 function nextQuestion () {
+
   if(timeRanOut  === 1) {
 	   $("section ul a").show();
-	   $("#correct img:last-child").remove();
-	   $("#incorrect img:last-child").remove();
-	   correct(quiz[1].correct);
-		questions(quiz[1].question);
-		var picture = "<img src='https://wildrovertours.com/wp-content/uploads/2015/12/Cliffs-of-Moher-5.jpg'>"
-        document.querySelector("#images").innerHTML = picture; 
-        answers(quiz[1].answer[0], quiz[1].answer[1], quiz[1].answer[2], quiz[1].answer[3]);   
-       
-}
+	   $("#correct").empty();
+	   $("#incorrect").empty()
+  	 questions(quiz[1].question);
+  	 var picture = "<img src='https://wildrovertours.com/wp-content/uploads/2015/12/Cliffs-of-Moher-5.jpg'>"
+     document.querySelector("#images").innerHTML = picture;
+     answers(quiz[1].answer[0], quiz[1].answer[1], quiz[1].answer[2], quiz[1].answer[3]);
+   }
 
-
-
-   if(timeRanOut ===2) {
-$("section ul a").show();
- $("#correct img:last-child").remove();
-	   $("#incorrect img:last-child").remove();
-        correct(quiz[2].correct);
-		questions(quiz[2].question);
-        answers(quiz[2].answer[0], quiz[2].answer[1], quiz[2].answer[2], quiz[2].answer[3]);
-        $("#images").hide();
-       
-        
-
-}
+ if(timeRanOut === 2) {
+   $("section ul a").show();
+   $("#correct img:last-child").remove();
+   $("#incorrect img:last-child").remove();
+	 questions(quiz[2].question);
+   answers(quiz[2].answer[0], quiz[2].answer[1], quiz[2].answer[2], quiz[2].answer[3]);
+   $("#images").hide();
+  }
 
   if(timeRanOut ===3 ) {
   	$("section ul a").show();
-  	 $("#correct img:last-child").remove();
-	   $("#incorrect img:last-child").remove();
-		correct(quiz[3].correct);
+  	$("#correct img:last-child").remove();
+	  $("#incorrect img:last-child").remove();
 		questions(quiz[3].question);
-        answers(quiz[3].answer[0], quiz[3].answer[1], quiz[3].answer[2], quiz[3].answer[3]);
-        
-
-}
+    answers(quiz[3].answer[0], quiz[3].answer[1], quiz[3].answer[2], quiz[3].answer[3]);
+  }
 
   if(timeRanOut === 4 ) {
   	$("section ul a").show();
- $("#correct img:last-child").remove();
-	   $("#incorrect img:last-child").remove();
-        correct(quiz[4].correct);
+    $("#correct img:last-child").remove();
+	  $("#incorrect img:last-child").remove();
 		questions(quiz[4].question);
 		var picture = "<img src='http://travelchannel.sndimg.com/content/dam/images/travel/fullset/2015/10/12/new-seven-wonders-machu-picchu.jpg.rend.tccom.616.462.jpeg'>"
-        document.querySelector("#images").innerHTML = picture; 
-        answers(quiz[4].answer[0], quiz[4].answer[1], quiz[4].answer[2], quiz[4].answer[3]);
-        $("#images").show();
-        
+    document.querySelector("#images").innerHTML = picture;
+    answers(quiz[4].answer[0], quiz[4].answer[1], quiz[4].answer[2], quiz[4].answer[3]);
+    $("#images").show();
+  }
 
-}
-
-if (timeRanOut ===5 ) {
-
- 	 $("#images").hide();
- 	 $("#questions").text("correct " + win + " missed " + (miss));
- 	 stop();
+  if (timeRanOut ===5 ) {
+   	 $("#images").hide();
+   	 $("#questions").text("correct " + win + " missed " + (miss));
+   	 stop();
      $("section").remove();
- 	 $("#timer").remove();
-}
+   	 $("#timer").remove();
+  }
 };
 //end content display conditionals
 
